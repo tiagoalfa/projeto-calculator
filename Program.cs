37e65b568e835a515f241ccb93924206bd9dc46d
tiagoalfa <64914744+tiagoalfa@users.noreply.github.com>
2020-08-12 20:06:35 -0300

    teste calculadora

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Historico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] historicos_calculadora = new int[6] { 2, 23, 12, 39, 44, 13 };
+            int valor;
+            valor = historicos_calculadora[3];
+            Console.WriteLine("O elemento é {0}", valor.ToString());
+            
+        }
+    }
+}
